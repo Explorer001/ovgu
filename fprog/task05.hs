3b8c55c309867414758b0d729ad3bc45e678f52f
Explorer001 <lukas.gehreke@gmail.com>
2020-11-28 13:33:09 +0100

    implemented task 5.4

--- fprog/task05.hs
+++ fprog/task05.hs
@@ -9,3 +9,20 @@ palin = filter (\x -> show x == reverse (show x)) [0 ..]
 
 partsum :: Num a => [a] -> [a]
 partsum xs = [x | x <- scanl (+) 0 xs]
+
+--
+-- 5.4 Folding
+--
+data Seq a = End | Seq a (Seq a) | Lseq [a] (Seq a) | Pseq (a,a) (Seq a) deriving (Show)
+
+instance Functor Seq where
+    fmap f (Seq v s) = Seq (f v) (fmap f s)
+    fmap f (Lseq v s) = Lseq (fmap f v) (fmap f s)
+    fmap f (Pseq (v1, v2) s) = Pseq (f v1, f v2) (fmap f s)
+    fmap f End = End
+
+instance Foldable Seq where
+    foldr f n (Seq v s) = f v (foldr f n s)
+    foldr f n (Lseq v s) = foldr f (foldr f n s) v
+    foldr f n (Pseq (v1, v2) s) = f v2 (f v1 (foldr f n s))
+    foldr f n End = n
